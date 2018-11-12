052f78b25df97038e9a7f7d430d1a3d95db064a1
Nikhil <nikhil.bhandari.74@gmail.com>
2018-11-12 16:58:33 +0530

    Added basic structure for frontend

---
+++ server/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+const PORT = 6090;
+
+app.use('/admin', express.static('client/admin'));
+app.use('/', express.static('client/app'));
+
+app.listen(PORT, () => {
+  console.log(`Server Listening on ${PORT}`);
+});
