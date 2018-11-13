5976bf125bc29b643fd3697584355157a48da86a
Nikhil <nikhil.bhandari.74@gmail.com>
2018-11-13 16:07:25 +0530

    Added splitChunks configuration

--- webpack.config.js
+++ webpack.config.js
@@ -3,27 +3,37 @@ const path = require('path');
 const adminConfig = {
   name: 'adminApp',
   entry: {
-    admin:  './client/admin/app.js',
-    vendor: ['lodash']
+    admin: './client/admin/app.js',
   },
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'client' ,'admin'),
+    path: path.resolve(__dirname, 'client', 'admin'),
   },
-  mode: 'production'
+  mode: 'production',
+  optimization: {
+    splitChunks: {
+      name: 'vendor',
+      chunks: 'all',
+    }
+  }
 };
 
 const appConfig = {
   name: 'publicApp',
   entry: {
     app: './client/app/app.js',
-    vendor: ['lodash']
   },
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'client' ,'app')
+    path: path.resolve(__dirname, 'client', 'app')
   },
-  mode: 'production'
+  mode: 'production',
+  optimization: {
+    splitChunks: {
+      name: 'vendor',
+      chunks: 'all',
+    }
+  }
 };
 
 module.exports = [adminConfig, appConfig];
